3d7efc8fb06203a25101c9176d745c8463232757
Adam <adamgiebl@gmail.com>
2020-01-06 20:58:12 +0100

    Updates

---
+++ precache-manifest.1f2968832003147704daf622bcab865b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1f295cb2061b5118691a59423ed532b",
+    "url": "/typie/index.html"
+  },
+  {
+    "revision": "c93f17f5272fa84b6ab4",
+    "url": "/typie/static/css/main.734292a3.chunk.css"
+  },
+  {
+    "revision": "16e1353aee1a369b1eb3",
+    "url": "/typie/static/js/2.41ca1d80.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/typie/static/js/2.41ca1d80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c93f17f5272fa84b6ab4",
+    "url": "/typie/static/js/main.022aed54.chunk.js"
+  },
+  {
+    "revision": "f6d2297ad9fe281297cc",
+    "url": "/typie/static/js/runtime-main.cfaa8769.js"
+  },
+  {
+    "revision": "940379d5311f5117a52cb73e1a71d91f",
+    "url": "/typie/static/media/giphy.940379d5.gif"
+  }
+]);
